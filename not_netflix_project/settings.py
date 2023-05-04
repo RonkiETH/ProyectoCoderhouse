f9c6b7aff622fa9f182a7ec9d80baed9c8b993d6
Ronki <ronki@gmail.com>
2023-05-03 21:37:23 -0300

    Segundo Commit

--- not_netflix_project/settings.py
+++ not_netflix_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+&%kmiv2vmti6hxw%nadp+nb9=cwh%ivh1b3wi45mb1%2+z8t&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
